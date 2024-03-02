f5b1a2b19ffa135d2882482656abb2ce25a25bc7
Khushal Nirwal <104513074+GeniusNirwal@users.noreply.github.com>
2024-03-02 17:34:15 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//  #include<bits/stdc++.h>
+// to include all the libaries
+using namespace std;
+
+int main() {
+  int x, y;
+  // Only defining the variables
+  cin >> x >> y;
+  // to take an input from the user
+  cout << "The value of x is " << x << "\tThe value of y is "<< y << endl; 
+  // cout to pring the output
+  // endl to end the output line
+  return 0;
+}
