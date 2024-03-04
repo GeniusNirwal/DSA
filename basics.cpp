000819f449b064934b2342bce727c6847e51554b
Khushal Nirwal <104513074+GeniusNirwal@users.noreply.github.com>
2024-03-04 02:48:09 +0000

    1.1 Switch Case

--- basics.cpp
+++ basics.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int age;
-  cout << "Enter your age: ";
-  cin >> age;
-  if(age<18){
-    cout << "Not eligible for the job\n";
-  }
-  else if(age <=54){
-    cout<< "Eligible for job\n";
-  }
-  else if(age<=57){
-    cout<<"Eligible for job, but retirement soon\n";
-  }
-  else if(age>57){
-    cout << "Retirement time\n";
+  int day;
+  cout <<"Enter the day number(1 to 7): ";
+  cin >>day;
+  //in C++, duplicates of case values are disallowed. Each case value must be distinct
+  // It is possible to nest switch cases but it is generally discouraged due to over complexity
+  switch(day){
+    case 1:
+      cout<<"Monday"<<endl;
+      break;
+    case 2:
+      cout <<"Tuesday"<<endl;
+      break;
+    case 3:
+      cout<<"Wednesday"<<endl;
+      break;
+    case 4:
+      cout<<"Thurday"<<endl;
+      break;
+    case 5:
+      cout<<"Friday"<<endl;
+      break;
+    case 6:
+      cout<<"Saturday"<<endl;
+      break;
+    case 7:
+      cout<<"Sunday"<<endl;
+      break;
+    default:
+      cout<<"An error has occured"<<endl;
   }
   return 0;
 }
