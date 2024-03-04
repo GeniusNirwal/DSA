a7905e58a55cdf3011f1dc07a4f0b28dce94700c
Khushal Nirwal <104513074+GeniusNirwal@users.noreply.github.com>
2024-03-04 02:36:42 +0000

    1.1 If else

--- basics.cpp
+++ basics.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-//  #include<bits/stdc++.h>
-// to include all the libaries
 using namespace std;
-
 int main() {
-  // only the characters within 2 spaces are considered as 1 string
-  // so to get a string with spaces we use getline
-  // char datatype works jut like in C
-  string str;
-  getline(cin, str);
-  cout << str;
-
+  int age;
+  cout << "Enter your age: ";
+  cin >> age;
+  if(age<18){
+    cout << "Not eligible for the job\n";
+  }
+  else if(age <=54){
+    cout<< "Eligible for job\n";
+  }
+  else if(age<=57){
+    cout<<"Eligible for job, but retirement soon\n";
+  }
+  else if(age>57){
+    cout << "Retirement time\n";
+  }
   return 0;
-  
 }
